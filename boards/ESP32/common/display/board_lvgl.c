cc1bd0e5d8e7eb7a41af52ac23fe809ff7f5a522
YangJie <yangjie6759@163.com>
2025-05-22 14:01:42 +0800

    Merge the display configurations of the ESP32 and T5AI platforms

--- boards/ESP32/common/display/board_lvgl.c
+++ boards/ESP32/common/display/board_lvgl.c
@@ -43,7 +43,7 @@
  * @param None
  * @return OPERATE_RET Initialization result, OPRT_OK indicates success
  */
-OPERATE_RET board_esp32_lvgl_init(void)
+OPERATE_RET tuya_lvgl_init(void)
 {
     OPERATE_RET rt = OPRT_OK;
 
@@ -112,7 +112,7 @@ OPERATE_RET board_esp32_lvgl_init(void)
  * @param None
  * @return OPERATE_RET Lock result, OPRT_OK indicates success
  */
-OPERATE_RET board_esp32_mutex_lock(void)
+OPERATE_RET tuya_lvgl_mutex_lock(void)
 {
     OPERATE_RET rt = OPRT_OK;
 
@@ -127,7 +127,7 @@ OPERATE_RET board_esp32_mutex_lock(void)
  * @param None
  * @return OPERATE_RET Lock result, OPRT_OK indicates success
  */
-OPERATE_RET board_esp32_mutex_unlock(void)
+OPERATE_RET tuya_lvgl_mutex_unlock(void)
 {
     OPERATE_RET rt = OPRT_OK;
 
