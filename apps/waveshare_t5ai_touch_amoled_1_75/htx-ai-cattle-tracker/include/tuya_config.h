01a34ad396e4d5690aadc3630c0e645b2f84ecd6
Hsuan Han Lai <huate.lai@tuya.com>
2025-10-10 16:46:14 +0800

    update pid to cattle

--- apps/waveshare_t5ai_touch_amoled_1_75/htx-ai-cattle-tracker/include/tuya_config.h
+++ apps/waveshare_t5ai_touch_amoled_1_75/htx-ai-cattle-tracker/include/tuya_config.h
@@ -23,7 +23,7 @@
  */
 
 #ifndef TUYA_PRODUCT_ID
-#define TUYA_PRODUCT_ID "p320pepzvmm1ghse"
+#define TUYA_PRODUCT_ID "awtowrksuozwqqmx"
 #endif
 
 #define TUYA_OPENSDK_UUID    "uuidxxxxxxxxxxxxxxxx"             // Please change the correct uuid
